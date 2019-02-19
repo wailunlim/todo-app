23b90be21c0f09efdaccc1a6a574e1f24076d390
wailunlim <wailun@u.nus.edu>
2019-02-19 14:05:06 +0800

    Add tests for todo

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @todo = Todo.new(title: "Example title", task: "Example task", tag: "example", completed: false)
+  end
+
+  test "should be valid" do
+    assert @todo.valid?
+  end
+
+  test "task should be present" do
+    @todo.task = " "
+    assert_not @todo.valid?
+  end
+
+  test "title should not be too long" do
+    @todo.title = "x" * 100
+    assert_not @todo.valid?
+  end
 end
