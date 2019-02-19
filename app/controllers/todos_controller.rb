bda685807809226acd74930f9537cf4880003f06
wailunlim <wailun@u.nus.edu>
2019-02-19 22:45:00 +0800

    Update controller methods

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,19 @@ class TodosController < ApplicationController
   def show
     @todo = Todo.find(params[:id])
   end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      # Successful save, handle this here
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def todo_params
+      params.require(:todo).permit(:title, :task, :tag)
+    end
 end
