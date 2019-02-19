5f9e16bf5e77d99e25a538bd1a2b1ed0d9570ad6
wailunlim <wailun@u.nus.edu>
2019-02-19 22:46:42 +0800

    Add basic integration test for adding a todo

---
+++ test/integration/add_todo_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class AddTodoTest < ActionDispatch::IntegrationTest
+
+  test "blank task is invalid" do
+    get new_todo_path
+    assert_no_difference 'Todo.count' do
+      post todos_path, params: { todo: { title: "Test",
+                                         task: "  ",
+                                         tag: "test" } }
+    end
+  end
+end
